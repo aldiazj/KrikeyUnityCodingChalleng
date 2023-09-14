4fce1fd9f5d0f7b37751600def9e37108d26718d
Alejandro Diaz <alejandrodiazjllo@gmail.com>
2023-09-14 00:22:58 -0500

    Added level system

---
+++ Space Invaders/Assets/_Game/Scripts/Runtime/ScriptableObjects/Level/LevelSO.cs
@@ -0,0 +1,15 @@
+﻿using Runtime.ScriptableObjects.Enemy;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Runtime.ScriptableObjects.Level
+{
+    [CreateAssetMenu(menuName = "Create LevelSO", fileName = "LevelSO", order = 0)]
+    public class LevelSO: ScriptableObject
+    {
+        public EnemySO[] enemiesCatalog;
+        public int rows;
+        public int enemiesPerRow;
+        public float initialSpeedInterval;
+    }
+}
